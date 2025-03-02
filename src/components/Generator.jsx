90a87bf274f71a927c73664f2e5d38ef0ed59288
Sans <sanskarkarki20@gmail.com>
2025-03-02 08:32:23 +0545

    hero seciton complete, generator section on going

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import { WORKOUTS } from '../utils/exercise'
+
+function Header(props) {
+  const { index, title, description } = props
+  return (
+    <div className='flex flex-col gap-4 '>
+      <div className='flex items-center gap-2 justify-center'>
+        <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+        <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+      </div>
+      <p className='text-sm sm:text-base mx-auto'>{description}</p>
+    </div>
+  )
+}
 
 const Generator = () => {
   return (
     <div>
-      
+      <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+
+        <Header index={'01'} title="Pick your Poison" description="Select the workout you wish to endure"></Header>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+          {Object.keys(WORKOUTS).map((type, typeIndex) => {
+            return (
+              <button className='bg-slate-950 border border-blue-400 py-4 rounded-lg duration-200 hover:border-blue-800' key={typeIndex}>
+                <p className='capitalize'>{type.replaceAll('_', " ")}</p>
+              </button>
+            )
+          })}
+        </div>
+
+        <Header index={'01'} title="Lock on targets" description="Select the muscles juged for annihilation."></Header>
+        <div className=''>
+          <div>
+            <p>Select muscles groups </p>
+          </div>
+
+
+        </div>
+
+      </SectionWrapper>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] mx-auto p-4'>
+      <div className='flex flex-col gap-4'>
+        <p className="text-xl font-semibold">IT'S TIME TO LEVEL UP</p>
+        <h1 className="upercase text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+          LIFT<span className="text-blue-400">LOG</span>
+        </h1>
+      </div>
+
+      <p className="max-w-lg text-sm md:text-base font-light">
+        By entering, I fully acknowledge the risks of becoming <span className="font-bold">an absolute unit</span>,
+        a <span className="font-bold">walking mass monster</span> afflicted with
+        <span className="font-bold">severe gym obsession</span> and the inability to fit through standard doorways.
+      </p>
+      <button className='px-8 py-4 rounded-md border border-blue-600 border-solid bg-slate-950 blueShadow'>Accept & Begin</button>
+
     </div>
+
   )
 }
 
